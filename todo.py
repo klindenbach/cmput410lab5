ccee95876eb7ee9b0b84bfdd7bab6f8e2feee1c9
Konrad lindenbach <klindenb@ualberta.ca>
2015-02-03 09:15:00 -0700

    finished main lab

---
+++ todo.py
@@ -0,0 +1,49 @@
+from flask import *
+import sqlite3
+
+app = Flask(__name__)
+app.debug = True
+
+DATABASE = 'tasks.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route('/')
+def list():
+    tasks = query_db("select category, description, priority from tasks")
+    print tasks
+    for task in tasks:
+        for field in task:
+                print field
+    return render_template('list.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    category = request.form['category']
+    description = request.form['description']
+    priority = request.form['priority']
+    query_db(
+            "insert into tasks (category, description, priority) values(?,?,?)",
+            [category, description, priority]
+    )
+    get_db().commit()
+    return redirect(url_for('list'))
+
+if __name__ == "__main__":
+    app.run()
